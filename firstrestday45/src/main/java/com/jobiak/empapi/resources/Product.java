63612b3d8259f1b94dd248d8b00da9d8c99eaab0
MohammedShaheed30 <smsvlit9295@gmail.com>
2022-05-16 20:20:59 +0530

    done

---
+++ firstrestday45/src/main/java/com/jobiak/empapi/resources/Product.java
@@ -0,0 +1,28 @@
+package com.jobiak.empapi.resources;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
+public class Product {
+
+	@GetMapping(value="/mobile",produces = MediaType.APPLICATION_JSON_VALUE)
+	public String showCatalog() {
+		return new String("Samsung E2FH,Dual Camera,32GB Memory,49,999,I got you");
+	}
+	@GetMapping("/search/{modelId}")
+	public String searchModel(@PathVariable(value="modelId")String modelId){
+	    return new String(modelId+"is available only in Blue and Black colors,can be delivered in 24 hours");
+	}
+	
+	
+	@GetMapping("/search/{modelId}/brand/{brand}")
+	public String searchModel(@PathVariable(value="modelId")String modelId,@PathVariable(value="brand")String brand) {
+	    return new String(modelId+brand+"Currently not available for your location");
+	}
+	
+}
